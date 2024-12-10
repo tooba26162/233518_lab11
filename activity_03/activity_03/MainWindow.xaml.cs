3103b1f848848496dc806a6606afe6cdea913013
tooba26162 <233518@students.au.edu.pk>
2024-12-10 14:26:47 +0500

    Add files via upload

---
+++ activity_03/activity_03/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace activity_03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e) 
+        { 
+            if (comboBox.SelectedItem != null) 
+            { 
+                ComboBoxItem selectedItem = comboBox.SelectedItem as ComboBoxItem; 
+                if (selectedItem != null) 
+                { 
+                    textBox.Text = selectedItem.Content.ToString(); 
+                } 
+            } 
+        }
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e) 
+        { 
+            if (comboBox1.SelectedItem != null) 
+            { 
+                ComboBoxItem selectedItem = comboBox1.SelectedItem as ComboBoxItem; 
+                if (selectedItem != null) 
+                { 
+                    textBox1.Text = selectedItem.Content.ToString(); 
+                } 
+            } 
+        }
+
+    }
+}
+
+    
+
